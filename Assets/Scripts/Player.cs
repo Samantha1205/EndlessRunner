36980ef09cca7413491e96218495ab4c1b2ab024
Samantha1205 <99206967+Samantha1205@users.noreply.github.com>
2023-01-20 21:51:04 -0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float jumpForce = 5;
+    [SerializeField] Rigidbody2D rb;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
