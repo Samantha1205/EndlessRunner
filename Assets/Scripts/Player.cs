72be95413498a44489d82676a4a4742c66222481
Samantha1205 <99206967+Samantha1205@users.noreply.github.com>
2023-03-21 00:10:21 -0400

    module7

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,33 @@ public class Player : MonoBehaviour
 {
     [SerializeField] float jumpForce = 5;
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] Transform raycastOrigin;
+    [SerializeField] bool isGrounded;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (isGrounded == true)
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            }
+        }
+
+        RaycastHit2D hit = Physics2D.Raycast(raycastOrigin.position, Vector2.down);
+
+        if (hit.collider != null)
+        {
+            if (hit.distance < 0.1f)
+            {
+                isGrounded = true;
+            }
+            else
+            {
+                isGrounded = false;
+            }
+            Debug.Log(hit.transform.name);
+            Debug.DrawRay(raycastOrigin.position, Vector2.down, Color.green);
         }
     }
 }
